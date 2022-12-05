8c58c8b017f036d240a46adb29385c5e3287c665
conPuppy <hoangnhatnam29102000@gmail.com>
2022-12-05 23:57:48 +0700

    doneNamesearch

--- src/model/Permission.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Permission {
-    USER,
-    ADMIN
-}
---
+++ src/model/comparatorbyprice/ComparatorPriceAscending.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Comparator;
+
+public class ProductComparatorByPrice implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        return (int) (o1.getPrice()- o2.getPrice());
+    }
+
+}
